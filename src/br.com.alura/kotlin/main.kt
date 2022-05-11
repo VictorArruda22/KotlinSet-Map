891932d513b8c78ca4e43ed6076cd5c748f3ff14
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-05-11 13:51:05 -0300

    Add .toList

--- src/br.com.alura/kotlin/main.kt
+++ src/br.com.alura/kotlin/main.kt
@@ -1,13 +1,14 @@
 fun main(){
     val banco = BancoDeNomes()
+    val nomesSalvos: Collection<String> = banco.nomes
 
     banco.salva("Alex")
-    println(banco.nomes)
+    println(nomesSalvos)
     println(BancoDeNomes().nomes)
 }
 
 class BancoDeNomes{
-    val nomes: Collection<String> get() = dados
+    val nomes: Collection<String> get() = dados.toList()
 
     fun salva(nome: String){
        dados.add(nome)
