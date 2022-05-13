5d19a987da978fc82c955f8ec97935aff9e5dc38
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-05-13 16:30:58 -0300

    Using Map with safecall

--- src/br.com.alura/kotlin/main.kt
+++ src/br.com.alura/kotlin/main.kt
@@ -1,7 +1,17 @@
 fun main(){
-    testaSet()
-}
+    val pedidos = mapOf(Pair(1, 20.0), 2 to 30.5)
+    println(pedidos)
+    val pedido = pedidos[3]
+    pedido?.let {
+        println("pedido $it")
+    }
+    for (pedido in pedidos){
+        println( pedido )
+        println("Número do pedido: ${pedido.key}")
+        println("Valor do pedido: ${pedido.value}")
+    }
 
+}
 
 
 
