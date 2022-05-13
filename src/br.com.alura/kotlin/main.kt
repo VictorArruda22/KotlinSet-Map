c75d3053898b8069243308d5f9941a806ab6bf8a
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-05-13 17:29:36 -0300

    Using Put

--- src/br.com.alura/kotlin/main.kt
+++ src/br.com.alura/kotlin/main.kt
@@ -1,16 +1,23 @@
 fun main(){
-    val pedidos = mapOf(Pair(1, 20.0), 2 to 30.5)
+    val pedidos: MutableMap<Int, Double> = mutableMapOf(Pair(1, 20.0), 2 to 30.5)
     println(pedidos)
     val pedido = pedidos[3]
     pedido?.let {
         println("pedido $it")
     }
-    for (pedido in pedidos){
+    for (p: Map.Entry<Int, Double> in pedidos){
         println( pedido )
-        println("Número do pedido: ${pedido.key}")
-        println("Valor do pedido: ${pedido.value}")
+        println("Número do pedido: ${p.key}")
+        println("Valor do pedido: ${p.value}")
     }
-
+    pedidos[3] = 70.0
+    println(pedidos)
+    pedidos.put(4, 25.0)
+    println(pedidos)
+    pedidos[1] = 10.0
+    println(pedidos)
+    pedidos.putIfAbsent(5, 50.0)
+    println(pedidos)
 }
 
 
