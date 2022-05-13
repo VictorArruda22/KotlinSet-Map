f93efc83dc02e2da927deea5cf63658626d3204b
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-05-13 13:05:44 -0300

    Using the .distinct()

--- src/br.com.alura/kotlin/main.kt
+++ src/br.com.alura/kotlin/main.kt
@@ -1,5 +1,12 @@
 fun main(){
-    testaCopia()
+    val assistiramCursoDeKotlin:List<String> = listOf<String>("Victor", "João", "Ronaldo", "Viviane")
+    val assistiramCursoDeAndroid:List<String> = listOf<String>("Alex", "Carlos", "Ronaldo", "Viviane")
+    val assistiramAmbos: MutableList<String> = mutableListOf()
+
+    assistiramAmbos.addAll(assistiramCursoDeKotlin)
+    assistiramAmbos.addAll(assistiramCursoDeAndroid)
+
+    println(assistiramAmbos.distinct())
 }
 
 
