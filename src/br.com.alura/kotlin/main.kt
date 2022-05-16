6ef253e672b61206d9871f2a20f6d23bd7ae3189
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-05-16 13:04:15 -0300

    Using associate

--- src/br.com.alura/kotlin/main.kt
+++ src/br.com.alura/kotlin/main.kt
@@ -1,7 +1,22 @@
 fun main(){
-    testaComportamentosMap()
+    val pedidos = listOf(
+        Pedido(1, 20.0),
+        Pedido(2, 30.0),
+        Pedido(3, 40.0),
+        Pedido(4, 52.0)
+    )
+    println(pedidos)
+    val pedidosMapeados = pedidos.associateBy { pedido -> pedido.numero }
+    println(pedidosMapeados)
 
+    val pedidosFreteGratis = pedidos.associateWith { pedido ->
+        pedido.valor > 50.0
+    }
+
+    println(pedidosFreteGratis)
 }
+
+data class Pedido(val numero: Int, val valor: Double)
 
 
 
