5d3079b5172afe6182d9c0194ab283d47c1244eb
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-05-16 10:52:12 -0300

    Using GET

--- src/br.com.alura/kotlin/main.kt
+++ src/br.com.alura/kotlin/main.kt
@@ -1,12 +1,28 @@
 fun main(){
-    val pedidos: MutableMap<Int, Double> = mutableMapOf(Pair(1, 20.0), 2 to 30.5)
+    val pedidos: MutableMap<Int, Double?> = mutableMapOf(
+        Pair(1, 20.0),
+        2 to 30.5,
+        3 to null
+    )
+    val valorPedido = pedidos.getValue(1)
     println(pedidos)
+    val mensagem = pedidos.getOrElse(4) {
+        "Pedido não encontrado"
+    }
+    println(mensagem)
+
+    pedidos.getOrDefault(1, 50.0)
+
+    println(pedidos.getOrDefault(0, -20.0))
+}
+
+fun testaMap(pedidos: MutableMap<Int, Double>) {
     val pedido = pedidos[3]
     pedido?.let {
         println("pedido $it")
     }
-    for (p: Map.Entry<Int, Double> in pedidos){
-        println( pedido )
+    for (p: Map.Entry<Int, Double> in pedidos) {
+        println(pedido)
         println("Número do pedido: ${p.key}")
         println("Valor do pedido: ${p.value}")
     }
