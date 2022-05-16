523e97d6273f9c905ac98151f7a6c92f4b3a7ca6
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-05-16 11:04:30 -0300

    Using Filter

--- src/br.com.alura/kotlin/main.kt
+++ src/br.com.alura/kotlin/main.kt
@@ -1,8 +1,11 @@
 fun main(){
-    val pedidos: MutableMap<Int, Double?> = mutableMapOf(
+    val pedidos: MutableMap<Int, Double> = mutableMapOf(
         Pair(1, 20.0),
         2 to 30.5,
-        3 to null
+        3 to 40.0,
+        4 to 100.0,
+        5 to 60.0,
+        6 to 25.0
     )
     val valorPedido = pedidos.getValue(1)
     println(pedidos)
@@ -14,6 +17,30 @@ fun main(){
     pedidos.getOrDefault(1, 50.0)
 
     println(pedidos.getOrDefault(0, -20.0))
+
+    for (numero in pedidos.keys){
+        println("Pedido: $numero")
+    }
+
+    for (valor in pedidos.values){
+        println("Valor do pedido: $valor")
+    }
+
+    val pedidosFiltrados = pedidos.filter { (numero, valor) ->
+        numero % 2 == 0 && valor > 40.0
+    }
+
+    val pedidosAcima = pedidos.filterValues { valor ->
+        valor > 70.0
+    }
+
+    val pedidosPares = pedidos.filterKeys { numero ->
+        numero % 2 == 0
+    }
+
+    println(pedidosAcima)
+    println(pedidosPares)
+    println(pedidosFiltrados)
 }
 
 fun testaMap(pedidos: MutableMap<Int, Double>) {
